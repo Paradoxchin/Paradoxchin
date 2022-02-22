054137df2008a4ad6b707405a3caa105ab0f607d
Paradoxchin <100197761+Paradoxchin@users.noreply.github.com>
2022-02-22 12:16:52 +0100

    Update Monster Spiel.py

--- Monster Spiel.py
+++ Monster Spiel.py
@@ -1,58 +1,58 @@
-from random import *                                                                            #importiert alles aus random
-import random                                                                                   #importiert random 
-import time                                                                                     #importiert die Zeit
+from random import *                                                                            
+import random                                                                                  
+import time                                                                                     
 
-monster_tür = 0                                                                                 #erstellt die Variable die anbibt hinter welcher tür sich das Monster versteckt
-player = 0                                                                                      #erstellt die Variable die vom Spieler veränder wird um die Tür auszuwählen
-i = "y"                                                                                         #erstellt die Variable die für die while-Schleife benötigt wird
-stage = 0                                                                                       #erstellt die Variable die das Level angibt
-list = ["Herr Jäger", "Frau Irobalieva", "Frau Lukasser", "Herr Bucher"]                        #erstellt eine Liste die vier verschiedene Monster enthält
-print("AI: Do you want to play a game? y/n: ", end = "")                                        #der Computer fragt ab ob du ein Spiel spielen möchtest
-x = input()                                                                                     #der Spielerinput ob er ein spiel spielen will oder nicht
+monster_tür = 0                                                                                 #creats a varible which contains the door where tho monster is hiding
+player = 0                                                                                      #creats a verible which contains the door that the player is choosing
+i = "y"                                                                                         #creats a verible that is needed for the while-loop
+stage = 0                                                                                       #creats a verible that contains the stage you'r on
+list = ["Ghost", "Phantom", "Butcher", "Doom Slayer"]                                           #creats a list that contains the monsters
+print("AI: Do you want to play a game? y/n: ", end = "")                                        #the computer asks if you wanna play
+x = input()                                                                                     #the player says if he wants to play or not
 
-if x == "y":                                                                                    #if-Abfrage ob der Spieler ein Spiel spielen will
-    Monster = random.choice(list)                                                               #es wird ein Monster aus der Liste zufällig gewählt
-    print(".")                                                                                  #immitiert eine Ladesequenz
-    time.sleep(0.5)                                                                             #der Ablauf des programmes wird für eine bestimmte Zeit pausiert
+if x == "y":                                                                                    #if-function if the player wants to play a game
+    Monster = random.choice(list)                                                               #a monster is randomly chosen from the list
+    print(".")                                                                                  #imitates a loading sequenz
+    time.sleep(0.5)                                                                             #the program freezes for a certain time
     print("..")
     time.sleep(0.5)
     print("...")
     time.sleep(0.5)
-    print("AI: You have to pick one of three doors by typing 1, 2 or 3 in the terminal.")       #dieser Print erklärt die Regeln des Spieles
+    print("AI: You have to pick one of three doors by typing 1, 2 or 3 in the terminal.")       #explains the rules to the player
     time.sleep(3)
     print("AI: Good Luck!")
     time.sleep(1)
     
-    while i == "y":                                                                             #die while-Schleife ermöglicht es mehrere Level zu haben
-        monster_tür = int(randint(1,3))                                                         #die Tür hinter der sich das Monster versteckt wird ausgewählt
-        stage = int(stage + 1)                                                                  #das aktuelle Level wird berechnet  
-        print("AI: Stage " + str(stage))                                                        #sagt dem Spieler in welchem Level er sich befindet
-        print("")                                                                               #Leerzeile
-        print("AI: Pick one of three doors. ", end = "")                                        #der Spieler wird aufgefordert eine von 3 Türen auszuwählen
-        player = int(input())                                                                   #der Spieler gibt die Tür seiner Wahl ein                                                 
+    while i == "y":                                                                             #the while-loop makes it possible to have more levels
+        monster_tür = int(randint(1,3))                                                         #the door behind which the monster is hiding is selected
+        stage = int(stage + 1)                                                                  #the current level is calculated
+        print("AI: Stage " + str(stage))                                                        #tells the player in which level he is
+        print("")                                                                               #space
+        print("AI: Pick one of three doors. ", end = "")                                        #the player is asked to choose one of 3 doors
+        player = int(input())                                                                   #the player enters the door of his choice                                                 
         
-        if player == monster_tür:                                                               #eine if-Abfrage die erkennt ob das Monster hinter der ausgewählten Tür befindet
-            time.sleep(0.5)                                                                     #immitiert eine Ladesequenz
+        if player == monster_tür:                                                               #a if-function that detects if the chosen door contains the monster
+            time.sleep(0.5)                                                                     #imitates a loading sequenz
             print(".")
             time.sleep(0.5)
             print("..")
             time.sleep(0.5)
             print("...")
             time.sleep(0.5)
-            print("AI: Sorry mate; but the monster " + Monster + " has found you.")             #dem Spieler wird mitgeteilt welches Monster ihn gefunden hat
+            print("AI: Sorry mate; but the " + Monster + " has found you.")                     #the game tells the player which monster has found him
             print("AI: You lost.")
-            print("AI: Your score was " + str(stage))                                           #teilt dem Spieler seine Punkteanzahl mit
-            break                                                                               #beendet die Schleife
+            print("AI: Your score was " + str(stage))                                           #tells the player his score
+            break                                                                               #ends the loop
         
-        if player >= 4 or player <= 0:                                                          #wenn der Spieler eine Tür wählt die es nicht gibt 
+        if player >= 4 or player <= 0:                                                          #if the player is choosing a door that not exists 
             time.sleep(0.5)
             print("")
-            while True:                                                                         #erstellt while-Schleife um den Cheater vollzuspammen
+            while True:                                                                         #creats a while-loop to spam the cheater
                 print("AI: Eat shit idiot. ", end="")                                           #tells the Player to eat shit for trying to cheat
-            break                                                                               #beendet die Schleife
+            break                                                                               #ends the loop
        
         else:
-            time.sleep(0.5)                                                                     #immitiert eine Ladesequenz
+            time.sleep(0.5)                                                                     #imitates a loading sequenz
             print(".")
             time.sleep(0.5)
             print("..")
@@ -61,7 +61,7 @@ if x == "y":                                                                    
             time.sleep(0.5)
 
 else:
-    print(".")                                                                                  #immitiert eine Ladesequenz
+    print(".")                                                                                  #imitates a loading sequenz
     time.sleep(0.5)
     print("..")
     time.sleep(0.5)
